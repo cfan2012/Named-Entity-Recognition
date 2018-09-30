130ca2490b99dfad40ea892766595eecd8feab47
cfan2012 <inon@vip.qq.com>
2018-09-30 15:26:13 +0800

    test

---
+++ sonna_run.sh
@@ -0,0 +1,5 @@
+sonar-scanner \
+  -Dsonar.projectKey=a84f34afc0c976bff8629ef3e53b7a0c77dff233 \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://tdpsonar.api.thoughtworks.net \
+  -Dsonar.login=xbtes
